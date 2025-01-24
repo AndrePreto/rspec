f5ab68c184e8edfe4ea5ae7e665da9ddda5e1d97
André Preto <andre_preto_69@hotmail.com>
2025-01-24 18:02:01 +0000

    implicit_and_explicit_subjects - Learning about implicit and explicit subjects within rspec test examples

--- spec/programming_language_spec.rb
+++ spec/programming_language_spec.rb
@@ -9,14 +9,14 @@ RSpec.describe ProgrammingLanguage do
     let(:language) { ProgrammingLanguage.new('Python') }
 
     it "should store the name of the programming language" do
-      expect(language.name).to eq('Python')
+      expect(language.name).to(eq('Python'))
     end
 
     context 'with no argument' do
       let(:language) { ProgrammingLanguage.new }
 
       it 'should return the default language name' do
-        expect(language.name).to eq('Ruby')
+        expect(language.name).to(eq('Ruby'))
       end
     end
 
